48a95bf18fed999fa4cdb542e44af467f90e132d
Michael Gardner <gardnm4@rpi.edu>
2018-02-13 11:44:38 -0500

    connected project and added packages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Demo from './demo';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Demo />
+  </BrowserRouter>,
+  document.querySelector('#root')
+);
